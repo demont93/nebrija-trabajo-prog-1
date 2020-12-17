82ae70a9fcfe702f58fc7c8424e93921548fee8b
demont93 <demont93@gmail.com>
2020-12-17 13:31:28 -0400

    Comentario explicando estructura del programa.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,40 @@
+// Reglas basicas:
+// La ruleta tiene 37 ranuras (0 - 36).
+// Pares rojos, Impares negros.
+// El 0 siempre pierde.
+// Color acertado: dinero x 2
+// Numero acertado: dinero x 35
+// 4 jugadores
 //
-// Created by demont93 on 12/17/20.
+// Orden de los jugadores:
+// Todos los jugadores juegan en orden hasta que se retiren o pierdan el dinero.
 //
+// Mostrar por pantalla al finalizar:
+// 1. Cuanto ha ganado/perdido la banca.
+// 2. Cuanto ha ganado/perdido cada jugador en orden.
+//
+// Etapas de cada turno:
+//  Etapa 1: Mostrar data inicial del jugador actual.
+//  Etapa 2: Pedir casilla o aceptar retiro.
+//  Etapa 3: Simular giro de la rueda con un "sleep".
+//  Etapa 4: Mostrar si ha acertado, que ha acertado
+//           Si se ha quedado sin dinero.
+//           Data actualizada.
+//
+// Mostrar por pantalla en cada etapa de cada turno:
+// Etapa 1:
+// 1. Etiqueta del jugador. Ej. Jugador 1.
+// 2. Dinero actual. Ej. e5
+// Etapa 2:
+// 1. Casilla y color elegido.
+// Etapa 3:
+// 1. Indicador de que la rueda esta girando. (posbile animacion para flair).
+// Etapa 4:
+// 1. Si ha acertado o no y que ha acertado. Ej. Acertaste color.
+// 2. Dinero actualizado.
+// 3. Si se ha quedado sin dinero.
 
+
+int main() {
+  return 0;
+}
